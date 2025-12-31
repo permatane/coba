bbd90c538dd6af2ae352194661851fba2e92f9b0
Matane <permatamailbox@gmail.com>
2025-12-31 15:48:54 +0700

    Update build.gradle.kts

--- Kazefuri/build.gradle.kts
+++ Kazefuri/build.gradle.kts
@@ -1,36 +1,26 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
+// use an integer for version numbers
+version = 2
 
-android {
-    namespace = "com.kazefuri"
-    compileSdk = 34
 
-    defaultConfig {
-        minSdk = 21
-    }
+cloudstream {
+    language = "id"
+    // All of these properties are optional, you can safely remove them
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
+    // description = "Lorem Ipsum"
+     authors = listOf("Matane")
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies {
-    implementation("com.lagradost:cloudstream3:4.6.0")
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "TvSeries",
+        "Anime",
+    )
+    isCrossPlatform = true
+    iconUrl = "https://www.google.com/s2/favicons?domain=sv3.kazefuri.cloud&sz=%size%"
 }
