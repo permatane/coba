605f70af354cdbb687561dec98a08427a0581404
Matane <permatamailbox@gmail.com>
2025-12-31 15:43:15 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.android.tools.build:gradle:8.13.0")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
