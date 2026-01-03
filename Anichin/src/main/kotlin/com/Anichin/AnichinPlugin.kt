de67c13b9b5965d6fc31f23fd8f8d8e2b35ca4df
Matane <permatamailbox@gmail.com>
2026-01-03 15:25:39 +0700

    Update AnichinPlugin.kt

--- Anichin/src/main/kotlin/com/Anichin/AnichinPlugin.kt
+++ Anichin/src/main/kotlin/com/Anichin/AnichinPlugin.kt
@@ -12,7 +12,6 @@ import com.lagradost.cloudstream3.extractors.Okrulink
 class AnichinPlugin : Plugin() {
     override fun load() {
         registerMainAPI(Anichin())
-        registerMainAPI(Kazefuri())
         registerExtractorAPI(embedwish())
         registerExtractorAPI(Filelions())
         registerExtractorAPI(VidHidePro5())
