66af51f045dad6f396ef245929ff8e3551814463
Matane <permatamailbox@gmail.com>
2026-01-13 13:57:14 +0700

    Update Donghuaword.kt

--- Donghua/src/main/kotlin/com/Matane/Donghuaword.kt
+++ Donghua/src/main/kotlin/com/Matane/Donghuaword.kt
@@ -8,93 +8,13 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
 
 
-class Donghuaword  : Anichin() {
+class Donghuaword  : Animekhor() {
     override var mainUrl              = "https://donghuaworld.com"
     override var name                 = "Donghuaword"
     override val hasMainPage          = true
     override var lang                 = "id"
     override val hasDownloadSupport   = true
     override val supportedTypes       = setOf(TvType.Movie, TvType.Anime)
-
-        override val mainPage = mainPageOf(
-        "anime/?status=ongoing&type=&order=update" to "Recently Updated",
-        "anime/?type=comic&order=update" to "Comic Recently Updated",
-        "anime/?type=comic" to "Comic Series",
-        "anime/?status=&type=ona&sub=&order=update" to "Donghua Recently Updated",
-        "anime/?status=&type=ona" to "Donghua Series",
-        "anime/?status=&sub=&order=latest" to "Latest Added",
-        "anime/?status=&type=&order=popular" to "Popular",
-        "anime/?status=completed&order=update" to "Completed",
-    )
-
-
-    private fun Element.toSearchquery(): SearchResponse {
-        val title     = this.select("div.bsx > a").attr("title")
-        val href      = fixUrl(this.select("div.bsx > a").attr("href"))
-        val posterUrl = fixUrlNull(this.selectFirst("div.bsx > a img")?.getsrcAttribute())
-        return newMovieSearchResponse(title, href, TvType.Movie) {
-            this.posterUrl = posterUrl
-        }
-    }
-    
-    override suspend fun search(query: String): List<SearchResponse> {
-        val searchResponse = mutableListOf<SearchResponse>()
-
-        for (i in 1..3) {
-            val document = app.get("${mainUrl}/page/$i/?s=$query").documentLarge
-
-            val results = document.select("div.listupd > article").mapNotNull { it.toSearchquery() }
-
-            if (!searchResponse.containsAll(results)) {
-                searchResponse.addAll(results)
-            } else {
-                break
-            }
-
-            if (results.isEmpty()) break
-        }
-
-        return searchResponse
-    }
-    
-    override suspend fun load(url: String): LoadResponse {
-        val document = app.get(url).documentLarge
-        val title= document.selectFirst("h1.entry-title")?.text()?.trim().toString()
-        val href=document.selectFirst(".eplister li > a")?.attr("href") ?:""
-        var poster = document.select("meta[property=og:image]").attr("content")
-        val description = document.selectFirst("div.entry-content")?.text()?.trim()
-        val type=document.selectFirst(".spe")?.text().toString()
-        val tvtag=if (type.contains("Movie")) TvType.Movie else TvType.TvSeries
-        return if (tvtag == TvType.TvSeries) {
-            val Eppage= document.selectFirst(".eplister li > a")?.attr("href") ?:""
-            val doc= app.get(Eppage).documentLarge
-            val epposter = doc.select("meta[property=og:image]").attr("content")
-            val episodes=doc.select("div.episodelist > ul > li").map { info->
-                        val href1 = info.select("a").attr("href")
-                        val episode = info.select("a span").text().substringAfter("-").substringBeforeLast("-")
-                        newEpisode(href1)
-                        {
-                            this.name=episode
-                            this.posterUrl=epposter
-                        }
-            }
-            newTvSeriesLoadResponse(title, url, TvType.Anime, episodes.reversed()) {
-                this.posterUrl = poster
-                this.plot = description
-            }
-        } else {
-            if (poster.isEmpty())
-            {
-                poster=document.selectFirst("meta[property=og:image]")?.attr("content")?.trim().toString()
-            }
-            newMovieLoadResponse(title, url, TvType.Movie, href) {
-                this.posterUrl = poster
-                this.plot = description
-            }
-        }
-    }
-
-    
 
     override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
         val document = app.get(data).documentLarge
@@ -110,3 +30,4 @@ class Donghuaword  : Anichin() {
         return true
     }
 }
+
