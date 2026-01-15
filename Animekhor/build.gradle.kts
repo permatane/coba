a32a72257a099b26fa051d495abed2cef076c1b3
Matane <permatamailbox@gmail.com>
2026-01-15 10:21:25 +0700

    Add files via upload

---
+++ Animekhor/build.gradle.kts
@@ -0,0 +1,23 @@
+// use an integer for version numbers
+version = 9
+
+
+cloudstream {
+    description = "Anime and Movies"
+    language    = "id"
+    authors = listOf("Phisher98")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 0 // will be 3 if unspecified
+
+    tvTypes = listOf("AnimeMovie","Anime")
+    iconUrl="https://www.google.com/s2/favicons?domain=https://animekhor.org/&sz=%size%"
+
+    isCrossPlatform = true
+}
