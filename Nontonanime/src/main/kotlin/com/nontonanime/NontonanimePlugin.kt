2b25dcc42a79e22fd6438fe3e8052264fa28eeaf
Matane <permatamailbox@gmail.com>
2026-01-15 13:08:52 +0700

    Update NontonanimePlugin.kt

--- Nontonanime/src/main/kotlin/com/nontonanime/NontonanimePlugin.kt
+++ Nontonanime/src/main/kotlin/com/nontonanime/NontonanimePlugin.kt
@@ -3,6 +3,7 @@ package com.Nontonanime
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.extractors.*
 import android.content.Context
 
 @CloudstreamPlugin
@@ -10,10 +11,14 @@ class NontonanimePlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(Nontonanime())
+        registerExtractorAPI(Okrulink())
+        registerExtractorAPI(Dailymotion())
         registerExtractorAPI(Nontonanimeid())
         registerExtractorAPI(EmbedKotakAnimeid())
         registerExtractorAPI(KotakAnimeidCom())
-        registerExtractorAPI(Gdplayer())
+        registerExtractorAPI(KotakAnimelink1())
+        registerExtractorAPI(KotakAnimelink2())
+        registerExtractorAPI(Gdriveplayer())
         registerExtractorAPI(Kotaksb())
     }
 }
